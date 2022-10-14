b305ca83932e28fa856f04d5fe203eaa256bb00c
Joe Fong <joe.fong@madetech.com>
2022-10-14 11:28:57 +0100

    try to use shouldThrow in test

--- src/Exercise/Ex01.hs
+++ src/Exercise/Ex01.hs
@@ -1,9 +1,12 @@
-module Exercise.Ex01 (myLast)  where
+module Exercise.Ex01 (myLast, myButLast)  where
 
 
 myLast :: Ord a => [a] -> a
 myLast lst = 
   case lst of
-    [] -> error "Exception: calling myLast on empty list"
+    [] -> error "Exception: empty list"
     [x] -> x
     (_:xs) -> myLast xs
+
+myButLast :: Ord a => [a] -> a
+myButLast _ = error "not implemented"
--- test/Exercise/Ex01Spec.hs
+++ test/Exercise/Ex01Spec.hs
@@ -1,11 +1,19 @@
 module Exercise.Ex01Spec (spec)  where
 
 import Test.Hspec
-import Exercise.Ex01 (myLast)
+import Exercise.Ex01 (myLast, myButLast)
+import Control.Exception (evaluate)
 
 spec :: Spec
 spec = do
   describe "myLast" $ do
-    it "return the last element in a list" $ do
+    it "Find the last element of a list." $ do
       myLast ([1,2,3,4] :: [Int]) `shouldBe` (4 :: Int)
       myLast ['x','y','z'] `shouldBe` 'z'
+      evaluate (myLast "") `shouldThrow` errorCall "Exception: empty list"
+  
+  describe "myButLast" $ do
+    it "Find the last but one element of a list." $ do
+      myButLast ([1,2,3,4] :: [Int]) `shouldBe` (3 :: Int)
+      myButLast ['a'..'z'] `shouldBe` 'y'
+      evaluate (myButLast ['a']) `shouldThrow` errorCall "foo"

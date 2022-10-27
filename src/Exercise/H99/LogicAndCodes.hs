df54be9f07ad5e8431d0bc736637836277221a63
Joe Fong <joe.fong@madetech.com>
2022-10-27 11:50:00 +0100

    done problem 48

--- src/Exercise/H99/LogicAndCodes.hs
+++ src/Exercise/H99/LogicAndCodes.hs
@@ -1,9 +1,48 @@
 module Exercise.H99.LogicAndCodes where
 
+import Control.Monad (replicateM)
+
 table :: (Bool -> Bool -> Bool) -> [[Bool]]
 table f = 
     [[a, b, f a b] | a <- [True, False], b <- [True, False]]
 
+and' :: Bool -> Bool -> Bool
+and' = (&&)
+
+or' :: Bool -> Bool -> Bool
+or' = (||)
+
+not' :: Bool -> Bool
+not' True = False
+not' False = True
+
+nand :: Bool -> Bool -> Bool
+nand a b = not' $ and' a b
+
+nor :: Bool -> Bool -> Bool
+nor a b = not' $ or' a b
+
+xor :: Bool -> Bool -> Bool
+xor True = not'
+xor False = id
+
+equ :: Bool -> Bool -> Bool
+equ a b = not' $ xor a b
+
+infixl 4 `or'`
+-- infixl 4 `nor`
+-- infixl 5 `xor`
+infixl 6 `and'`
+-- infixl 6 `nand`
+infixl 3 `equ`
 
 printTruthTable :: [[Bool]] -> IO ()
-printTruthTable tbl = mapM_ putStrLn $ [unwords $ show <$> bools | bools <- tbl]
+printTruthTable tbl = mapM_ putStrLn $ [makeRow bools | bools <- tbl]
+  where
+    makeRow :: [Bool] -> String
+    makeRow [] = ""
+    makeRow [x, y] = show x ++ " => " ++ show y
+    makeRow (x:xs) = show x ++ " " ++ makeRow xs
+
+tablen :: Int -> ([Bool] -> Bool) -> [[Bool]]
+tablen n f = [args ++ [f args] | args <- replicateM n [True, False]]
--- test/Exercise/H99/LogicAndCodesSpec.hs
+++ test/Exercise/H99/LogicAndCodesSpec.hs
@@ -13,9 +13,13 @@ spec :: Spec
 spec = do
   describe "table" $ do
     it "returns a truth table of a given logical expression in two variables" $ do
-      let inputFunc = (\a b -> and' a (or' a b))
-           where
-            and' = (&&)
-            or' = (||)
+      let inputFunc a b = and' a (or' a b)
       let expected = [[True, True, True], [True, False, True], [False, True, False], [False, False, False]]
       table inputFunc `shouldBe` expected
+
+  describe "tablen" $ do
+    it "returns a truth table of a given logical expression in n variables" $ do
+      let expected = [[True, True, True, True], [True, True, False, True], [True, False, True, True], [True, False, False, True], [False, True, True, True], [False, True, False, True], [False, False, True, True], [False, False, False, True]]
+      let actual = tablen 3 (\[a, b, c] -> a `and'` (b `or'` c) `equ` a `and'` b `or'` a `and'` c)
+      actual `shouldBe` expected
+      

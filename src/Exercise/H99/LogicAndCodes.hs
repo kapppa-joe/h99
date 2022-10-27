69350d08e1246eecee51252d249a520e0408a4cc
Joe Fong <joe.fong@madetech.com>
2022-10-27 11:03:51 +0100

    add print function for problem 46

--- src/Exercise/H99/LogicAndCodes.hs
+++ src/Exercise/H99/LogicAndCodes.hs
@@ -3,3 +3,7 @@ module Exercise.H99.LogicAndCodes where
 table :: (Bool -> Bool -> Bool) -> [[Bool]]
 table f = 
     [[a, b, f a b] | a <- [True, False], b <- [True, False]]
+
+
+printTruthTable :: [[Bool]] -> IO ()
+printTruthTable tbl = mapM_ putStrLn $ [unwords $ show <$> bools | bools <- tbl]
